42642968d677a6875d3a7c4a32728795b9fed339
Ivan <aiven715@gmail.com>
2020-05-03 00:47:46 +0300

    Input css

--- packages/components/src/Input/index.js
+++ packages/components/src/Input/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import cx from "classnames"
+import styles from "./index.css"
 
 export default ({
   size = "normal",
@@ -10,38 +11,18 @@ export default ({
   ...props
 }) => (
   <>
-    <div className="relative">
+    <div className={styles.root}>
       <input
         className={cx(
-          root,
-          sizes[size],
-          shouldFitContainer && fullContainer,
-          error ? errorBorder : border
+          styles.element,
+          styles[size],
+          shouldFitContainer && styles.full,
+          error && styles.error
         )}
         {...props}
       />
-      {iconAfter && <IconAfter value={iconAfter} />}
+      {iconAfter && <div className={styles.iconAfter}>{iconAfter}</div>}
     </div>
-    {error && <div className="text-xs text-error mt-1">{error}</div>}
+    {error && <div className={styles.errorText}>{error}</div>}
   </>
 )
-
-const IconAfter = ({ value }) => (
-  <div className="absolute top-0 bottom-0 right-0 mr-3 flex items-center">
-    <div className="w-3">{value}</div>
-  </div>
-)
-
-const root =
-  "focus:outline-none focus:bg-white rounded text-gray-shade-110 placeholder-gray border bg-gray-tint-130"
-const border =
-  "border-gray-tint-50 focus:border-gray-tint-10 focus:shadow-gray-outline"
-// const errorBorder = "border-error-shade-100 focus:shadow-error-outline"
-const errorBorder = "border-gray-shade-100"
-const fullContainer = "w-full"
-
-const sizes = {
-  compact: "h-6 px-1",
-  normal: "h-8 px-2",
-  loose: "h-10 px-3",
-}
