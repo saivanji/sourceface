592c201671e2f6759ca333ea905e6689c8e68759
Ivan <aiven715@gmail.com>
2020-05-04 15:19:21 +0300

    Get rid of webpack config

--- packages/config/client/webpack-plugins.js
+++
@@ -1,8 +0,0 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-
-module.exports = [
-  new MiniCssExtractPlugin({
-    filename: "[name].css",
-    chunkFilename: "[id].css",
-  }),
-]
