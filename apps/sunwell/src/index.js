6bd58cf4f6b2259814c7bde97c4d3f32ddc2cd47
Ivan <aiven715@gmail.com>
2020-05-04 17:40:04 +0300

    Comments

--- apps/sunwell/src/index.js
+++ apps/sunwell/src/index.js
@@ -9,10 +9,6 @@ ReactDOM.render(<pages.SignIn />, document.getElementById("root"))
 // - Connect fonts
 // - Setup dynamic imports
 // - Setup hot module replacement
-// - Keep generated css in the external file
 //
 // - Implement Text component
 // - Implement Space component?
-//
-// - Minification of components css
-// - Most likely remove client webpack configuration from config and have separate configuration for components and app
