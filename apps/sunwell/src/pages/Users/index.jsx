dc835b2f2c7e95c75aa8653e29d5e47b3f12d0dc
Ivan <aiven715@gmail.com>
2020-05-11 00:35:30 +0300

    Use card. Improve table

--- apps/sunwell/src/pages/Users/index.jsx
+++ apps/sunwell/src/pages/Users/index.jsx
@@ -1,28 +1,35 @@
 import React from "react"
 import { InviteForm } from "components/administration"
+import Badge from "@sourceface/components/badge"
 import Input from "@sourceface/components/input"
-import Tabs, { Tab } from "@sourceface/components/tabs"
+import Tabs, { Tab, TabsHead, TabsBody } from "@sourceface/components/tabs"
 import Search from "./assets/search.svg"
 import styles from "./index.css"
 
-// take into the account gray bg of content area??
-// or bg of content area will be white?
 export default () => {
   return (
     <div className={styles.wrap}>
       <Tabs className={styles.tabs}>
-        <Tab isSelected>Users</Tab>
-        <Tab>Invitations</Tab>
+        <TabsHead>
+          <Tab isSelected>Users</Tab>
+          <Tab iconAfter={<Badge value="30" />}>Invitations</Tab>
+        </TabsHead>
+        <TabsBody>
+          <div className={styles.header}>
+            <Input
+              className={styles.search}
+              type="text"
+              placeholder="Search for a user"
+              iconBefore={<Search />}
+            />
+            <InviteForm className={styles.inviteForm} />
+          </div>
+          Adipisicing blanditiis suscipit exercitationem maiores quis doloremque
+          Aut iusto perferendis consequuntur tempora mollitia? Numquam
+          voluptatibus eligendi assumenda commodi necessitatibus. Quisquam quae
+          quaerat ex praesentium amet Corporis explicabo vitae soluta vel!
+        </TabsBody>
       </Tabs>
-      <div className={styles.header}>
-        <Input
-          className={styles.search}
-          type="text"
-          placeholder="Search for a user"
-          iconBefore={<Search />}
-        />
-        <InviteForm className={styles.inviteForm} />
-      </div>
     </div>
   )
 }
