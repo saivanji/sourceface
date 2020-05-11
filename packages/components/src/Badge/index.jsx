22e4d44c8dff8c378498f1628c30a9c75e5a32a9
Ivan <aiven715@gmail.com>
2020-05-11 13:00:32 +0300

    Badge appearance and shape

--- packages/components/src/Badge/index.jsx
+++ packages/components/src/Badge/index.jsx
@@ -1,4 +1,9 @@
 import React from "react"
+import cx from "classnames"
 import styles from "./index.css"
 
-export default ({ value }) => <span className={styles.root}>{value}</span>
+export default ({ value, appearance = "dark", shape = "rounded" }) => (
+  <span className={cx(styles.root, styles[appearance], styles[shape])}>
+    {value}
+  </span>
+)
--- packages/components/src/Badge/index.stories.jsx
+++ packages/components/src/Badge/index.stories.jsx
@@ -1,8 +1,17 @@
 import React from "react"
-import { withKnobs, text } from "@storybook/addon-knobs"
+import { withKnobs, text, select } from "@storybook/addon-knobs"
 import { withA11y } from "@storybook/addon-a11y"
 import Badge from "./index"
 
+const makeProps = () => ({
+  shape: select("Shape", ["rounded", "squared"], "rounded"),
+})
+
 export default { title: "Badge", decorators: [withKnobs, withA11y] }
 
-export const Regular = () => <Badge value={text("Value", "25")} />
+export const Light = () => (
+  <Badge {...makeProps()} appearance="light" value={text("Value", "25")} />
+)
+export const Dark = () => (
+  <Badge {...makeProps()} appearance="dark" value={text("Value", "25")} />
+)
