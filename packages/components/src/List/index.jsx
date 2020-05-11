f1cd3b4f1eb52ad9dcbe1153949b63c4e4f80841
Ivan <aiven715@gmail.com>
2020-05-11 13:24:19 +0300

    classname

--- packages/components/src/List/index.jsx
+++ packages/components/src/List/index.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import cx from "classnames"
 import styles from "./index.css"
 
 export default function List({ children, className }) {
   return <div className={className}>{children}</div>
 }
 
-export function Item({ children }) {
-  return <div className={styles.item}>{children}</div>
+export function Item({ children, className }) {
+  return <div className={cx(styles.item, className)}>{children}</div>
 }
