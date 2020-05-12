f3cf869080725dca926f8f2db9da41c8f07c4751
Ivan <aiven715@gmail.com>
2020-05-13 02:43:44 +0300

    Avatar classname

--- packages/components/src/Avatar/index.jsx
+++ packages/components/src/Avatar/index.jsx
@@ -1,4 +1,7 @@
 import React from "react"
+import cx from "classnames"
 import styles from "./index.css"
 
-export default ({ value }) => <div className={styles.root}>{value}</div>
+export default ({ value, className }) => (
+  <div className={cx(styles.root, className)}>{value}</div>
+)
