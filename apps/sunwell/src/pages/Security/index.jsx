fe4a5805d35a53fe36f2a35cfaea229c208ae00b
Ivan <aiven715@gmail.com>
2020-05-14 15:07:20 +0300

    Security page layout

--- apps/sunwell/src/pages/Security/index.jsx
+++ apps/sunwell/src/pages/Security/index.jsx
@@ -1,22 +1,26 @@
 import React from "react"
 import Button from "@sourceface/components/button"
+import Card from "@sourceface/components/card"
 import Input from "@sourceface/components/input"
 import Label from "@sourceface/components/label"
+import { Layout } from "components/common"
 import styles from "./index.css"
 
 export default () => {
   return (
-    <>
-      <Label className={styles.label} title="Old password">
-        <Input placeholder="********" type="password" />
-      </Label>
-      <Label className={styles.label} title="New password">
-        <Input placeholder="********" type="password" />
-      </Label>
-      <Label className={styles.label} title="New password confirmation">
-        <Input placeholder="********" type="password" />
-      </Label>
-      <Button type="submit">Update password</Button>
-    </>
+    <Layout>
+      <Card>
+        <Label className={styles.label} title="Old password">
+          <Input placeholder="********" type="password" />
+        </Label>
+        <Label className={styles.label} title="New password">
+          <Input placeholder="********" type="password" />
+        </Label>
+        <Label className={styles.label} title="New password confirmation">
+          <Input placeholder="********" type="password" />
+        </Label>
+        <Button type="submit">Update password</Button>
+      </Card>
+    </Layout>
   )
 }
