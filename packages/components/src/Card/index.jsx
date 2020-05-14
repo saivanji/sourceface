ca2f4d285f2acb634dfe0a05c4dc20bbe4be3fcf
Ivan <aiven715@gmail.com>
2020-05-14 15:06:06 +0300

    Classname of card

--- packages/components/src/Card/index.jsx
+++ packages/components/src/Card/index.jsx
@@ -2,6 +2,6 @@ import React from "react"
 import cx from "classnames"
 import styles from "./index.css"
 
-export default ({ children, size = "normal" }) => (
-  <div className={cx(styles.root, styles[size])}>{children}</div>
+export default ({ children, className, size = "normal" }) => (
+  <div className={cx(styles.root, styles[size], className)}>{children}</div>
 )
