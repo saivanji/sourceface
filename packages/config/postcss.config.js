be1010e5c5c34c50d39a4f56dca601a8e748d467
Ivan <aiven715@gmail.com>
2020-05-18 16:22:15 +0300

    Do not use postcss import plugin and remove custom properties setting

--- packages/config/postcss.config.js
+++ packages/config/postcss.config.js
@@ -1,18 +1,11 @@
-const atImport = require("postcss-import")
 const autoprefixer = require("autoprefixer")
 const presetEnv = require("postcss-preset-env")
 
 module.exports = {
   plugins: [
-    atImport(),
     autoprefixer,
     presetEnv({
       stage: 0,
-      features: {
-        "custom-properties": {
-          preserve: false,
-        },
-      },
     }),
   ],
 }
