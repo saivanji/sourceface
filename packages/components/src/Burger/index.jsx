8973c7aafbc26eebfb5fa190e8278f773880299e
Ivan <aiven715@gmail.com>
2020-05-18 23:48:42 +0300

    Burger appearance

--- packages/components/src/Burger/index.jsx
+++ packages/components/src/Burger/index.jsx
@@ -1,8 +1,9 @@
 import React from "react"
 import cx from "classnames"
-import MenuIcon from "assets/menu.svg"
+import MenuIcon from "./assets/menu.svg"
 import styles from "./index.scss"
 
+// TODO: implement compact size
 export default function Burger({ className, appearance = "light" }) {
   return (
     <div className={cx(styles.root, styles[appearance], className)}>
--- packages/components/src/Header/index.jsx
+++ packages/components/src/Header/index.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import cx from "classnames"
 import styles from "./index.scss"
 
+// TODO: implement compact size
 // TODO: implement dark appearance
 export default function Header({ children, className }) {
   return <div className={cx(styles.root, className)}>{children}</div>
