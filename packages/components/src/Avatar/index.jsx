b5ccfa204821df93b90d10306a7110924eea95e6
Ivan <aiven715@gmail.com>
2020-05-24 22:53:36 +0300

    Fix react duplicate version

--- packages/components/src/Avatar/index.jsx
+++ packages/components/src/Avatar/index.jsx
@@ -2,8 +2,17 @@ import React from "react"
 import cx from "classnames"
 import styles from "./index.scss"
 
-export default ({ value, size = "normal", className, ...props }) => (
-  <div {...props} className={cx(styles.root, styles[size], className)}>
+export default ({
+  size = "normal",
+  mode = "light",
+  value,
+  className,
+  ...props
+}) => (
+  <div
+    {...props}
+    className={cx(styles.root, styles[mode], styles[size], className)}
+  >
     {value}
   </div>
 )
