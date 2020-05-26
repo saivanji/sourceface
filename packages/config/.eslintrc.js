f12ea67c83f5ee6694fa42865a92136cb13ef98a
Ivan <aiven715@gmail.com>
2020-05-26 15:54:21 +0300

    Resolve conflicts

--- packages/config/.eslintrc.js
+++ packages/config/.eslintrc.js
@@ -4,12 +4,17 @@ module.exports = {
     es6: true,
     node: true,
   },
+  extends: ["eslint:recommended"],
   overrides: [
     {
       files: ["*.ts?(x)"],
       parser: "@typescript-eslint/parser",
       plugins: ["@typescript-eslint", "react"],
       extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+      rules: {
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+      },
     },
   ],
   rules: {
