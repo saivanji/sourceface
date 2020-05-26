b2d3931339268376f0b5174aa77fcf2c3c4dd925
Ivan <aiven715@gmail.com>
2020-05-26 15:44:39 +0300

    Revert "Remove typescript import plugin"
    This reverts commit 152199bd52ebf1e0a5b36d659938ec97942714b0.

--- packages/config/.eslintrc.js
+++ packages/config/.eslintrc.js
@@ -10,7 +10,16 @@ module.exports = {
   rules: {
     quotes: ["error", "double"],
     "comma-dangle": "off",
+    // "import/no-unresolved": ["error", { commonjs: true }],
     "react/jsx-uses-react": "error",
     "react/jsx-uses-vars": "error",
   },
+  // settings: {
+  //   "import/resolver": {
+  //     node: {
+  //       extensions: [".ts", ".tsx"],
+  //       moduleDirectory: ["node_modules", "src/"],
+  //     },
+  //   },
+  // },
 }
