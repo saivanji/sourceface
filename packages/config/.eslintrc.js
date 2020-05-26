ccf8ff759ca11c7cdfb1af1c39bcee73eab9dd5d
Ivan <aiven715@gmail.com>
2020-05-26 15:52:22 +0300

    Ts overrides

--- packages/config/.eslintrc.js
+++ packages/config/.eslintrc.js
@@ -4,22 +4,16 @@ module.exports = {
     es6: true,
     node: true,
   },
-  parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  plugins: ["@typescript-eslint", "react"],
+  overrides: [
+    {
+      files: ["*.ts?(x)"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint", "react"],
+      extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    },
+  ],
   rules: {
     quotes: ["error", "double"],
     "comma-dangle": "off",
-    // "import/no-unresolved": ["error", { commonjs: true }],
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
   },
-  // settings: {
-  //   "import/resolver": {
-  //     node: {
-  //       extensions: [".ts", ".tsx"],
-  //       moduleDirectory: ["node_modules", "src/"],
-  //     },
-  //   },
-  // },
 }
