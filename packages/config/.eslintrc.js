152199bd52ebf1e0a5b36d659938ec97942714b0
Ivan <aiven715@gmail.com>
2020-05-26 15:27:32 +0300

    Remove typescript import plugin

--- packages/config/.eslintrc.js
+++ packages/config/.eslintrc.js
@@ -10,16 +10,7 @@ module.exports = {
   rules: {
     quotes: ["error", "double"],
     "comma-dangle": "off",
-    // "import/no-unresolved": ["error", { commonjs: true }],
     "react/jsx-uses-react": "error",
     "react/jsx-uses-vars": "error",
   },
-  // settings: {
-  //   "import/resolver": {
-  //     node: {
-  //       extensions: [".ts", ".tsx"],
-  //       moduleDirectory: ["node_modules", "src/"],
-  //     },
-  //   },
-  // },
 }
