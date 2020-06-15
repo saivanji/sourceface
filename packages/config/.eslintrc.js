310234b08254e26dd7f04e9ceea21a3ab6e2d9d9
Ivan <aiven715@gmail.com>
2020-06-15 17:38:43 +0300

    Add parser options

--- packages/config/.eslintrc.js
+++ packages/config/.eslintrc.js
@@ -8,16 +8,15 @@ module.exports = {
   rules: {
     quotes: ["error", "double"],
     "comma-dangle": "off",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
   },
   overrides: [
     {
       files: ["*.ts?(x)"],
       parser: "@typescript-eslint/parser",
+      parserOptions: { sourceType: "module" },
       plugins: ["@typescript-eslint", "react"],
-      rules: {
-        "react/jsx-uses-react": "error",
-        "react/jsx-uses-vars": "error",
-      },
     },
   ],
 }
