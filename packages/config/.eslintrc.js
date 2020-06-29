5e1ebe1dc084ea88a5b32cddbb8f280ae5b7e30c
Ivan <aiven715@gmail.com>
2020-06-29 17:25:34 +0300

    Remove ts

--- packages/config/.eslintrc.js
+++ packages/config/.eslintrc.js
@@ -5,18 +5,27 @@ module.exports = {
     node: true,
   },
   extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["import", "react"],
   rules: {
     quotes: ["error", "double"],
     "comma-dangle": "off",
+    "import/no-unresolved": ["error", { commonjs: true }],
     "react/jsx-uses-react": "error",
     "react/jsx-uses-vars": "error",
   },
-  overrides: [
-    {
-      files: ["*.ts?(x)"],
-      parser: "@typescript-eslint/parser",
-      parserOptions: { sourceType: "module" },
-      plugins: ["@typescript-eslint", "react"],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx"],
+        moduleDirectory: ["node_modules", "src/"],
+      },
     },
-  ],
+  },
 }
