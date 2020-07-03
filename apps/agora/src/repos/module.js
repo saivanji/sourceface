95442e25b186c0286c52faa2634556dbf3dd1e9f
Ivan <aiven715@gmail.com>
2020-07-03 14:43:43 +0300

    Update module

--- apps/agora/src/repos/module.js
+++ apps/agora/src/repos/module.js
@@ -1,7 +1,18 @@
-export const all = async pg => pg.manyOrNone(sql.all)
+export const all = async pg => await pg.manyOrNone(sql.all)
+export const one = async (moduleId, pg) => await pg.one(sql.one, [moduleId])
+export const updateConfig = async (moduleId, config, pg) =>
+  await pg.one(sql.updateConfig, [moduleId, config])
 
 const sql = {
   all: `
     SELECT * FROM modules
+    ORDER BY id ASC
+  `,
+  one: `
+    SELECT * FROM modules WHERE id = $1
+  `,
+  updateConfig: `
+    UPDATE modules SET config = $2 WHERE id = $1
+    RETURNING *
   `,
 }
--- apps/agora/src/schema/module/resolvers.js
+++ apps/agora/src/schema/module/resolvers.js
@@ -4,8 +4,26 @@ const modules = async (parent, args, { pg }) => {
   return await moduleRepo.all(pg)
 }
 
+const updateModule = async (parent, { moduleId, key, value }, { pg }) => {
+  return await pg.task(async t => {
+    const module = await moduleRepo.one(moduleId, t)
+
+    // TODO: perform validation of config input data depending on module type
+    // also check on validationSchema whether it has "key" user tries to update
+
+    return await moduleRepo.updateConfig(
+      moduleId,
+      { ...module.config, [key]: value },
+      t
+    )
+  })
+}
+
 export default {
   Query: {
     modules,
+  },
+  Mutation: {
+    updateModule,
   },
 }
