563e48b8fd9254b7b5c5a93130003e1f669d8739
Ivan <aiven715@gmail.com>
2020-07-06 17:41:15 +0300

    Roadmap comment

--- apps/sunwell/src/packages/runtime/index.js
+++ apps/sunwell/src/packages/runtime/index.js
@@ -31,10 +31,15 @@ export const evaluate = (input, scope) => {
 // There are 3 types in the language. String or Integer(Primitives. Function accepts them as arguments along with constants). Complex type is a return type of a function. It's not accepted as arguments.
 // Functions accept either constants or literal as input.
 // Runtime returns various syntax exceptions
+//
+// -- Roadmap --
 // There will be a conception of a map. Map is an object. functions are already accepting it as a single argument. Also replace prefix conception by a map
 // There will be ability to define a function as following:
 // x, z -> x.y
 // There will be arithmetic operations introduced with constants and primitives(number), in place where we can put value(constant or literal) we can put arithmetic operation since it's return type is a value
+// Think of having generic way for providing string and other data type input, since it's inconsistent for the user to have 2 inputs(template and expression)
+//   - If braces are provided - then consider as template, otherthise as expression?
+// Think of having conditions inside of templates
 //
 // -- Conceptions --
 // Expression - is a function call returning a complex type or expression is a Value
