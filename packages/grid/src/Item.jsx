a64f5e9131aea08b3fdc8a7d2657c9f6ab74beba
Ivan <aiven715@gmail.com>
2020-07-09 04:10:43 +0300

    WIP. Custom drag preview and placeholder

--- packages/grid/src/Item.jsx
+++ packages/grid/src/Item.jsx
@@ -1,49 +1,28 @@
 import React from "react";
-import * as utils from "./utils";
 
-const itemWidth = 3;
-const itemHeight = 4;
-
-export default function Item({
-  isDragging,
-  x,
-  y,
-  cols,
-  rowHeight,
-  containerWidth,
-  onDragStart,
-  onDragEnd
-}) {
-  console.log(isDragging);
-
+export default function Item({ style, onDrag, onDragStart, onDragEnd }) {
   return (
     <div
       draggable
       onDragStart={event => {
+        hidePreviewImage(event);
         return onDragStart && onDragStart(event);
       }}
+      onDrag={onDrag}
       onDragEnd={onDragEnd}
       style={{
-        transition: "transform .15s ease-out",
+        // transition: "transform .15s ease-out",
         position: "absolute",
         zIndex: 1,
-        backgroundColor: isDragging ? "lightGray" : "darkcyan",
-        height: utils.calcYCSS(itemHeight, rowHeight),
-        ...(!containerWidth
-          ? {
-              width: utils.calcXCSSPercentage(itemWidth, cols),
-              top: utils.calcYCSS(y, rowHeight),
-              left: utils.calcXCSSPercentage(x, cols)
-            }
-          : {
-              width: utils.calcXCSS(itemWidth, cols, containerWidth),
-              transform: `translate(${utils.calcXCSS(
-                x,
-                cols,
-                containerWidth
-              )}px, ${utils.calcYCSS(y, rowHeight)}px)`
-            })
+        backgroundColor: "darkCyan",
+        ...style
       }}
     />
   );
 }
+
+const hidePreviewImage = event => {
+  const img = new Image();
+  img.src = "";
+  event.dataTransfer.setDragImage(img, 0, 0);
+};
--- packages/grid/src/index.js
+++ packages/grid/src/index.js
@@ -5,6 +5,23 @@ import * as utils from "./utils";
 import Item from "./Item";
 import Lines from "./Lines";
 
+const item = {
+  height: 3,
+  width: 2
+};
+
+const items = [
+  {
+    id: "john",
+    text: "John",
+    x: 0,
+    y: 0,
+    height: 3,
+    width: 2,
+    color: "darkCyan"
+  }
+];
+
 function Grid() {
   const [{ x, y }, setPosition] = useState({ x: 0, y: 0 });
   const [isDragging, setDragging] = useState(false);
@@ -15,6 +32,7 @@ function Grid() {
   const rowHeight = 80;
   const rows = 10; // infinite?
   const cols = 8;
+  const containerWidth = container.current?.width;
 
   return (
     <>
@@ -47,15 +65,35 @@ function Grid() {
         style={{ position: "relative", height: 500 }}
       >
         <Item
-          isDragging={isDragging}
-          x={x}
-          y={y}
-          containerWidth={container.current?.width}
-          rows={rows}
-          cols={cols}
-          rowHeight={rowHeight}
+          style={{
+            height: utils.calcYCSS(item.height, rowHeight),
+            width: !containerWidth
+              ? utils.calcXCSSPercentage(item.width, cols)
+              : utils.calcXCSS(item.width, cols, containerWidth),
+            ...(!isDragging &&
+              (!containerWidth
+                ? {
+                    top: utils.calcYCSS(y, rowHeight),
+                    left: utils.calcXCSSPercentage(x, cols)
+                  }
+                : {
+                    transform: `translate(${utils.calcXCSS(
+                      x,
+                      cols,
+                      containerWidth
+                    )}px, ${utils.calcYCSS(y, rowHeight)}px)`
+                  }))
+          }}
+          onDrag={e => {
+            e.target.style.transform = `translate(${
+              e.pageX - container.current.element.offsetLeft
+            }px, ${e.pageY - container.current.element.offsetTop}px)`;
+          }}
           onDragStart={() => setDragging(true)}
-          onDragEnd={() => setDragging(false)}
+          onDragEnd={e => {
+            e.target.style.transform = "";
+            setDragging(false);
+          }}
         />
         {isDragging && container.current && (
           <Lines
@@ -63,25 +101,25 @@ function Grid() {
             rows={rows}
             cols={cols}
             rowHeight={rowHeight}
-            containerWidth={container.current.width}
+            containerWidth={containerWidth}
           />
         )}
       </div>
     </>
   );
 }
+// Definition:
+// Drag preview - either original dimmed appearance, static placeholder image or colored bg (persisting dimensions) or generic card with static size. Most likely will have static size card with type of module
+// When drag preview reaches more than half of a cell - move it there
+// When dragging keep original dimensions of a dragging element in a placeholder regardless how drag preview will look like
 
 // TODO:
-// Define UI approach of DnD
-//
-// Customize drag preview element
+// Custom drag preview element
 // Implement drag handle
 // Define root grid component interface
 //
 // Consider source boundaries while drag
 // Move element only when half area was hovered
-//
-// If draggable item is having lot of rows, limit them to a static value
 //
 // Have stacking and free movement at the same time
 
