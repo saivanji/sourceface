32d2f60629edeb611a29517eff447379a50c424c
Ivan <aiven715@gmail.com>
2020-07-12 22:41:07 +0300

    Refactor

--- packages/grid/src/useResizable.js
+++ packages/grid/src/useResizable.js
@@ -60,21 +60,39 @@ const listen = (
 ) => {
   node.onmousedown = e => {
     const { translateX, translateY } = getTransform(element);
+    const startX = e.clientX;
+    const startY = e.clientY;
+    const initialWidth = element.offsetWidth;
+    const initialHeight = element.offsetHeight;
 
-    const payload = {
-      startX: e.clientX,
-      startY: e.clientY,
-      minWidth,
-      minHeight,
-      horizontalBoundary,
-      verticalBoundary,
-      initialWidth: element.offsetWidth,
-      initialHeight: element.offsetHeight,
-      initialX: translateX,
-      initialY: translateY
+    const move = e => {
+      const deltaX = e.clientX - startX;
+      const deltaY = e.clientY - startY;
+      const isNorth = position === "nw" || position === "ne";
+      const isWest = position === "nw" || position === "sw";
+
+      const [w, x] = change(
+        isWest,
+        deltaX,
+        translateX,
+        initialWidth,
+        minWidth,
+        horizontalBoundary
+      );
+      const [h, y] = change(
+        isNorth,
+        deltaY,
+        translateY,
+        initialHeight,
+        minHeight,
+        verticalBoundary
+      );
+
+      element.style.width = `${w}px`;
+      element.style.height = `${h}px`;
+
+      element.style.transform = `translate(${x}px, ${y}px)`;
     };
-
-    const move = e => render(element, calculate(e, position, payload));
 
     const cleanup = e => {
       document.removeEventListener("mouseup", cleanup);
@@ -92,47 +110,6 @@ const listen = (
   };
 };
 
-const calculate = (
-  e,
-  position,
-  {
-    startX,
-    startY,
-    initialWidth,
-    initialHeight,
-    minWidth,
-    minHeight,
-    horizontalBoundary,
-    verticalBoundary,
-    initialX,
-    initialY
-  }
-) => {
-  const deltaX = e.clientX - startX;
-  const deltaY = startY - e.clientY;
-  const isNorth = position === "nw" || position === "ne";
-  const isWest = position === "nw" || position === "sw";
-
-  const [w, x] = change(
-    isWest,
-    deltaX,
-    initialX,
-    initialWidth,
-    minWidth,
-    horizontalBoundary
-  );
-  const [h, y] = change(
-    isNorth,
-    -deltaY,
-    initialY,
-    initialHeight,
-    minHeight,
-    verticalBoundary
-  );
-
-  return [w, h, x, y];
-};
-
 const change = (cond, delta, initialOffset, initialSize, minSize, boundary) => {
   if (cond) {
     return [
@@ -145,12 +122,4 @@ const change = (cond, delta, initialOffset, initialSize, minSize, boundary) => {
     range(initialSize + delta, minSize, boundary - initialOffset),
     initialOffset
   ];
-};
-
-const render = (element, [w, h, x, y]) => {
-  element.style.width = `${w}px`;
-  // biggest(h, minHeight)
-  element.style.height = `${h}px`;
-
-  element.style.transform = `translate(${x}px, ${y}px)`;
 };
