f889f1ce5a2291fb39918abd7b806f8dc1231a94
Ivan <aiven715@gmail.com>
2020-09-03 00:19:50 +0300

    Conditionally using DndProvider

--- apps/sunwell/src/packages/grid/Provider.jsx
+++ apps/sunwell/src/packages/grid/Provider.jsx
@@ -1,17 +1,33 @@
 import React, { createContext, useContext } from "react"
-import { DndProvider } from "react-dnd"
+import { DndProvider, DndContext } from "react-dnd"
 import { TouchBackend } from "react-dnd-touch-backend"
 import Layer from "./Layer"
 
 const context = createContext({ isWrapped: false })
 
 export default function GrillProvider({ children }) {
+  const { dragDropManager } = useContext(DndContext)
+  const wrappedDnd = !!dragDropManager
+
+  /**
+   * Do not wrapping in DndProvider if it's already wrapped from above.
+   */
   return (
     <context.Provider value={{ isWrapped: true }}>
-      <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
-        {children}
-        <Layer />
-      </DndProvider>
+      {!wrappedDnd ? (
+        <DndProvider
+          backend={TouchBackend}
+          options={{ enableMouseEvents: true }}
+        >
+          {children}
+          <Layer />
+        </DndProvider>
+      ) : (
+        <>
+          {children}
+          <Layer />
+        </>
+      )}
     </context.Provider>
   )
 }
