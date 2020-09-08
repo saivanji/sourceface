96c0153cd9133560c89cba839812381f18447c5f
Ivan <aiven715@gmail.com>
2020-09-08 03:58:41 +0300

    Add "end" prop

--- packages/components/src/Label/index.jsx
+++ packages/components/src/Label/index.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import styles from "./index.scss"
 
-export default ({ title, children }) => (
+// TODO: whether using "end" is correct?
+export default ({ title, children, end }) => (
   <div>
-    <div>
+    <div className={styles.line}>
       <span className={styles.title}>{title}</span>
+      {end && <div className={styles.end}>{end}</div>}
     </div>
     {children}
   </div>
