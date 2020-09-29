74d678fb72ddd17a7b404e531a3dc80f82fa2f9b
Ivan <aiven715@gmail.com>
2020-09-29 18:51:20 +0300

    Todos

--- apps/sunwell/src/packages/toolkit/components/Expression/index.jsx
+++ apps/sunwell/src/packages/toolkit/components/Expression/index.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import styles from "./index.scss"
 
+// TODO: if value is array, then act as a pipline, othewise single element
+// TODO: when created new item in a pipeline, have "acc/result/prev ->" as a placeholder
 export default ({ error, ...props }) => (
   <>
     <div className={styles.root}>
--- apps/sunwell/src/packages/toolkit/components/Option/index.jsx
+++ apps/sunwell/src/packages/toolkit/components/Option/index.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react"
 import { Label } from "@sourceface/components"
 import Braces from "assets/braces.svg"
 
+// TODO: most likely feature of using expression to compute value of every option is reduntant,
+// since for most cases values of these options are going to be customized statically instead of
+// at a runtime. For example option to have the pagination available or not is rarely needed to be
+// configured at a runtime.
 export default function Field({ children, title }) {
   return (
     <Label
