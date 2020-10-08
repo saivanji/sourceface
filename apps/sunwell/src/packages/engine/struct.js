f42bd57d05042c677e3135e1bb39da7c57566bd7
Ivan <aiven715@gmail.com>
2020-10-08 18:01:18 +0300

    Move tests

--- apps/sunwell/src/packages/engine/struct.js
+++ apps/sunwell/src/packages/engine/struct.js
@@ -1,6 +1,13 @@
 export const tokenize = input => {}
 
+// TODO: a.b.c is not identifier, choose another name for it
 /**
  * Syntax code analysis and parsing to a structure.
  */
 export const parse = (input, options) => {}
+
+//
+// tokenize - LexicalError
+// syntax validation - SyntaxError
+// parse - ?
+// execute - TypeError/ReferenceError/EvaluationError/ExecutionError?
