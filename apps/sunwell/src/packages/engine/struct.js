1e8a453990c6ff24ee0ad9b4c01312d9b4a8c2f1
Ivan <aiven715@gmail.com>
2020-10-08 03:34:24 +0300

    Addition

--- apps/sunwell/src/packages/engine/struct.js
+++ apps/sunwell/src/packages/engine/struct.js
@@ -1,4 +1,4 @@
-// TODO: if function result is the same as result of identifier and literal then logically it can be given to another function arguments(since identifiers and literals could). Think how to restrict that. Make function higher abstraction than identifiers and literals. Find similar example in js tokenisation.
+// TODO: if function result is the same as result of identifier and literal then logically it can be given to another function arguments(since identifiers and literals could). Think how to restrict that. Make function higher abstraction than identifiers and literals. Find similar example in js tokenisation.(object definition at a program root)
 
 export const tokenize = input => {}
 
