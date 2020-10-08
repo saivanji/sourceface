eefd1003f35af7a90145ecdb11c96fb4d8843da9
Ivan <aiven715@gmail.com>
2020-10-08 18:21:55 +0300

    Restructure

---
+++ apps/sunwell/src/packages/engine/parse.js
@@ -0,0 +1,9 @@
+// TODO: a.b.c is not identifier, choose another name for it
+/**
+ * Syntax code analysis and parsing to a structure.
+ */
+export const parse = (input, options) => {}
+
+// tokenize - LexicalError
+// parse - SyntaxError
+// execute - TypeError/ReferenceError/EvaluationError/ExecutionError?
---
+++ apps/sunwell/src/packages/engine/tokenize.js
@@ -0,0 +1,13 @@
+// TODO: if function result is the same as result of identifier and literal then logically it can be given to another function arguments(since identifiers and literals could). Think how to restrict that. Make function higher abstraction than identifiers and literals. Find similar example in js tokenisation.(object definition at a program root)
+
+export const tokenize = input => {}
+
+// TODO: a.b.c is not identifier, choose another name for it
+/**
+ * Syntax code analysis and parsing to a structure.
+ */
+export const parse = (input, options) => {}
+
+// tokenize - LexicalError
+// parse - SyntaxError
+// execute - TypeError/ReferenceError/EvaluationError/ExecutionError?
