4009fbd6984c1f55f08d8642cfcd62bc55c8acce
Ivan <aiven715@gmail.com>
2020-10-24 00:00:23 +0300

    Arguments cleanup

--- apps/sunwell/src/packages/actions/components/Arguments/index.jsx
+++ apps/sunwell/src/packages/actions/components/Arguments/index.jsx
@@ -1,91 +1,19 @@
-import React, { useState } from "react"
-import { Button } from "@sourceface/components"
-import Add from "assets/add.svg"
+import React from "react"
 import Action from "../Action"
 import Value from "../Value"
-import Snippet from "../Snippet"
+import Static from "../Static"
 import styles from "./index.scss"
 
+// TODO: will have key/value split in the dropdown
 export default function Arguments() {
-  const [creation, setCreation] = useState(null)
-
-  const stopCreation = () => setCreation(null)
-
   return (
     <Action.Section title="Input">
       <Action.SectionRow>
-        {!creation ? (
-          <div className={styles.creators}>
-            <AddButton onClick={() => setCreation({ type: "key" })}>
-              Add key
-            </AddButton>
-            <AddButton
-              onClick={() =>
-                setCreation({
-                  type: "group",
-                  value: { type: "local", name: "" },
-                })
-              }
-            >
-              Add group
-            </AddButton>
-          </div>
-        ) : creation.type === "key" ? (
-          <>
-            <Snippet
-              autoFocus
-              color="gray"
-              value={creation.key}
-              onChange={(key) => setCreation((data) => ({ ...data, key }))}
-              onDestroy={stopCreation}
-            />
-            {creation.key && !creation.value ? (
-              <AddButton
-                onClick={() =>
-                  setCreation((data) => ({
-                    ...data,
-                    value: { type: "local", name: "" },
-                  }))
-                }
-              >
-                Add value
-              </AddButton>
-            ) : (
-              !!creation.value && (
-                <Value
-                  autoFocus
-                  value={creation.value}
-                  onDestroy={() =>
-                    setCreation((data) => ({
-                      ...data,
-                      value: undefined,
-                    }))
-                  }
-                />
-              )
-            )}
-          </>
-        ) : (
-          creation.type === "group" && (
-            <Value
-              autoFocus
-              value={creation.value}
-              onChange={(value) => setCreation((data) => ({ ...data, value }))}
-              onDestroy={stopCreation}
-            />
-          )
-        )}
+        <div className={styles.creators}>
+          <Static creationTitle="Add key/value" />
+          <Value creationTitle="Add group" />
+        </div>
       </Action.SectionRow>
     </Action.Section>
   )
 }
-
-function AddButton({ children, onClick }) {
-  return (
-    <Button onClick={onClick} size="small" appearance="link" icon={<Add />}>
-      {children}
-    </Button>
-  )
-}
-
-function Creation() {}
--- apps/sunwell/src/packages/actions/components/Value/index.jsx
+++ apps/sunwell/src/packages/actions/components/Value/index.jsx
@@ -1,16 +1,20 @@
 import React from "react"
 import { Toggle, Autocomplete } from "@sourceface/components"
 import Snippet from "../Snippet"
+import Placeholder from "../Placeholder"
 
-// TODO: use + [action] in combination with dropdown
 // TODO: do not have switch for literal/variable, adding literal will be implemented by displaying
 // "Use 'something' as literal" option at the first position of dropdown.
-// TODO: Move "Snippet" code here. Accept "type" prop to understand if we should ask for static value or variable/literal.
 // TODO: remove icons, have only colors for variable types/literals. Display icons in dropdown instead.
-// TODO: use dropdown with similar appearance when adding a new action.
-export default function Value({ autoFocus, value, onChange, onDestroy }) {
+export default function Value({
+  value,
+  autoFocus,
+  creationTitle = "Add value",
+  onChange,
+  onDestroy,
+}) {
   const trigger = !value ? (
-    <Placeholder>Add value</Placeholder>
+    <Placeholder>{creationTitle}</Placeholder>
   ) : value.type === "literal" ? (
     <Snippet color="beige">{value.data}</Snippet>
   ) : (
