e6cb1f2211dfca7adffc51896fca3fc7318fa38c
Ivan <aiven715@gmail.com>
2020-10-25 03:50:14 +0300

    TODO

--- apps/sunwell/src/packages/factory/index.js
+++ apps/sunwell/src/packages/factory/index.js
@@ -1,4 +1,5 @@
 // TODO: think how to move actions and modules to a separate packages and make them to import factory. Do not import actions and modules from factory
+// TODO: imagine all app component hierarchy
 
 export { default as Container } from "./container"
 export { list as modules, getDefaultConfig } from "./modules"
