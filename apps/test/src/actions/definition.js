8b227a313dce5d5bc5e0f7c04ea422d2efff0a26
Ivan <aiven715@gmail.com>
2021-01-07 05:33:21 +0300

    Comments

---
+++ apps/test/src/actions/definition.js
@@ -0,0 +1,1 @@
+// Provide ability to define function to make reusable parts so they can be used in loops and so on. Can have multiple functions pipe here. Action result might be also a function not only a variable.
