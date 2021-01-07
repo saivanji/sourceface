f283fc365cc81bd7a35920440a76189a449e37eb
Ivan <aiven715@gmail.com>
2021-01-07 18:26:43 +0300

    Add comment

--- apps/test/src/actions/definition.js
+++ apps/test/src/actions/definition.js
@@ -1,1 +1,1 @@
-// Provide ability to define function to make reusable parts so they can be used in loops and so on. Can have multiple functions pipe here. Action result might be also a function not only a variable.
+// Provide ability to define function to make reusable parts so they can be used in loops and so on. Can have multiple functions pipe here. Action result might be also a function definition! not only a value. 2 types - value(variable, function call) and function definition
