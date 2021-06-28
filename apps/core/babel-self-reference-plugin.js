52213e295dd113bea3484f93f7eb532337d980a0
Ivan <aiven715@gmail.com>
2021-06-29 01:40:18 +0300

    Comments and prefix

--- apps/core/babel-self-reference-plugin.js
+++ apps/core/babel-self-reference-plugin.js
@@ -2,20 +2,26 @@ const { basename: getBasename } = require("path");
 const { default: template } = require("@babel/template");
 const types = require("@babel/types");
 
+const prefix = "__babel_self";
+
 module.exports = function () {
   return {
     visitor: {
       CallExpression(path) {
         const calleeName = path.node.callee.name;
 
-        // skipping object call expressions
+        /**
+         * Skipping object call expressions
+         */
         if (!calleeName) {
           return;
         }
 
         const binding = path.scope.getBinding(calleeName);
 
-        // skipping calls of functions not defined in the same file
+        /**
+         * Skipping calls of functions not defined in the same file
+         */
         if (!binding) {
           return;
         }
@@ -23,18 +29,24 @@ module.exports = function () {
         const parentPath = binding.path.parentPath;
 
         if (
-          // ignoring non export declarations
+          /**
+           * Ignoring non export declarations
+           */
           !parentPath.isExportDeclaration() ||
-          // ignoring default export declarations
+          /**
+           * Ignoring default export declarations
+           */
           parentPath.isExportDefaultDeclaration() ||
-          // ignoring "export {}" constructions, TODO: implement
+          /**
+           * Ignoring "export {}" constructions, TODO: implement
+           */
           parentPath.node.specifiers?.length > 0
         ) {
           return;
         }
 
         path.node.callee = types.memberExpression(
-          types.identifier("__self"),
+          types.identifier(prefix),
           types.identifier(calleeName)
         );
 
@@ -45,7 +57,7 @@ module.exports = function () {
           if (this.replaced) {
             const basename = getBasename(filename);
             const selfImport = template.ast(`
-              import * as __self from "./${basename}";
+              import * as ${prefix} from "./${basename}";
             `);
 
             path.unshiftContainer("body", selfImport);
