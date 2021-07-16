90ec6ffd94b201865c938a07c42347b706d2c72a
Ivan <aiven715@gmail.com>
2021-07-17 02:16:10 +0300

    Comment

--- apps/core/src/store/index.js
+++ apps/core/src/store/index.js
@@ -4,6 +4,7 @@
 // - do not need to catch in store. catching should happen on the level above. computation either succeeds, or fails with Interruption
 //   when interrupted in "value" - throw Interruption("value error message"), in dictionary - throw Interruption({ foo: "error message" })
 //   in future, will have "scope.error" when catch clause is selected, containing information from Interruption
+// - when stages could be failed apart from validation?
 //
 // - should we differ callbacks and read settings? for example have them under different keys. read under "fields" and callbacks under "callbacks"?
 // - error handling
