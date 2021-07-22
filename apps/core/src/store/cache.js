3b094f0fe5eaf7feb3f3e730137ddbe6e00ff9fd
Ivan <aiven715@gmail.com>
2021-07-22 17:01:44 +0300

    Correct

--- apps/core/src/store/cache.js
+++ apps/core/src/store/cache.js
@@ -84,7 +84,7 @@ export default class Cache {
       clearTimeout(timeout);
     }
 
-    // TODO: set invalidation timeout only after data$ has no subscribers
+    // TODO: set removal timeout only after data$ has no subscribers
     // and remove timeout if at least one subscriber appears.
 
     /**
